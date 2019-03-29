54637c4e60c8d8867c8a637463baf86301f229be
ayunas <ayunas@gmail.com>
2019-03-29 12:08:22 -0500

    mapped through the Array data in StarWarsJSON and for each individual object in the JSON array, returned a <p>with the text of that particular value at the name key of the object.

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -7,7 +7,15 @@ class StarWars extends React.Component {
     render() {
         
         return (
-            <div>{StarWarsJSON[0].name}</div>
+            <div>
+            {StarWarsJSON[0].name}
+            {StarWarsJSON.map( character => {
+            return <p>{character.name}</p>
+            })}
+            
+            
+            </div>
+            
         );
     }
 }
